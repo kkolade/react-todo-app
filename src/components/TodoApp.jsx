df411b95f97e19a7829c3f15176c763b947f5136
Kola Kolade <kolakolade@gmail.com>
2023-02-28 10:02:33 +0100

    Folder structure and files for the app created

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoApp() {
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <p>I am in a React Component!</p>
+    </>
+  );
+}
+
+export default TodoApp;
